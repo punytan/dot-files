12c8d25f1ee4b2c99da3ce7872e94764b031c8f2
punytan <punytan@gmail.com>
2011-04-24 18:16:32 +0900

    add libpng12-dev for Imager

--- build-env/01_apt.sh
+++ build-env/01_apt.sh
@@ -6,7 +6,7 @@ apt-get upgrade -f -y;
 apt-get build-dep perl -y;
 apt-get install build-essential ssh htop vim git-core screen -y;
 apt-get install unzip global ctags curl spell strace sysstat tree -y;
-apt-get install libpcre3-dev libssl-dev expat libxml2-dev libjpeg8-dev libgif-dev -y;
+apt-get install libpcre3-dev libssl-dev expat libxml2-dev libjpeg8-dev libgif-dev libpng12-dev -y;
 apt-get install daemontools-run -y;
 apt-get install arno-iptables-firewall -y;
 
