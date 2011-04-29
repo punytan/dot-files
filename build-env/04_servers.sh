4e4332457edef62c3756542b8bc81a0a5e95b1b9
punytan <punytan@gmail.com>
2011-04-30 05:03:23 +0900

    add mysql-client library

--- build-env/04_servers.sh
+++ build-env/04_servers.sh
@@ -2,7 +2,7 @@
 
 # server
 apt-get install nginx -y;
-apt-get install mysql-server-5.1 -y;
+apt-get install mysql-server-5.1 libmysqld-dev -y;
 
 # Kyoto Tycoon
 cd /tmp && wget http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.51.tar.gz && tar zxvf kyotocabinet-1.2.51.tar.gz && cd kyotocabinet-1.2.51 && ./configure && make && make check && make install;
