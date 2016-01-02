71eee78b91f9c0b704cb8ac5c982f24ae5fe14b6
punytan <punytan@gmail.com>
2016-01-02 14:08:26 +0900

    add vim-go

--- build-env/05_vimbundle.sh
+++ build-env/05_vimbundle.sh
@@ -23,6 +23,7 @@ case $1 in
         git clone git://github.com/wting/rust.vim.git;
         git clone git@github.com:derekwyatt/vim-scala.git;
         git clone git@github.com:toyamarinyon/vim-swift.git;
+        git clone git://github.com/fatih/vim-go.git;
         # utility
         git clone git://github.com/Shougo/neocomplcache.git;
         git clone git://github.com/thinca/vim-quickrun.git;
