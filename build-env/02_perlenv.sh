02e2b6d39acb9621209bcc80014919355c0bf751
punytan <punytan@gmail.com>
2019-03-16 16:52:47 +0900

    update perlbrew setup

--- build-env/02_perlenv.sh
+++ build-env/02_perlenv.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-curl -L http://xrl.us/perlbrewinstall | bash;
+TARGET_VERSION="perl-5.28.1"
+NCPU=`sysctl -n hw.ncpu`
+
+curl -L https://install.perlbrew.pl | bash;
 
 ~/perl5/perlbrew/bin/perlbrew init;
 
 source ~/perl5/perlbrew/etc/bashrc;
 
-perlbrew install -j 5 perl-5.12.4 &&
+perlbrew install -j "$NCPU" "$TARGET_VERSION" &&
 
-perlbrew switch perl-5.12.4 &&
+perlbrew switch "$TARGET_VERSION" &&
 
 perlbrew install-cpanm
 
