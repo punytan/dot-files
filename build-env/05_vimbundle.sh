86c9366e64ba0e9c6023fd67d0ca8d3fefa45d8e
punytan <punytan@gmail.com>
2022-07-26 21:49:17 +0900

    update vim bundle

--- build-env/05_vimbundle.sh
+++ build-env/05_vimbundle.sh
@@ -9,24 +9,23 @@ cd "$HOME/dot-files/vim/bundle";
 case $1 in
     'install')
         # color scheme
-        git clone git://github.com/thinca/vim-guicolorscheme.git;
-        git clone git://github.com/altercation/vim-colors-solarized.git;
+        git clone https://github.com/thinca/vim-guicolorscheme.git;
+        git clone https://github.com/altercation/vim-colors-solarized.git;
         # git stuff
-        git clone git://github.com/tpope/vim-fugitive.git;
-        git clone git://github.com/airblade/vim-gitgutter.git;
+        git clone https://github.com/tpope/vim-fugitive.git;
+        git clone https://github.com/airblade/vim-gitgutter.git;
         # syntax highlight
-        git clone git://github.com/groenewege/vim-less.git;
-        git clone git://github.com/hallison/vim-markdown.git;
-        git clone git://github.com/jnwhiteh/vim-golang.git;
-        git clone git://github.com/wting/rust.vim.git;
-        git clone git://github.com/fatih/vim-go.git;
-        git clone git@github.com:leafgarland/typescript-vim.git;
+        git clone https://github.com/hallison/vim-markdown.git;
+        git clone https://github.com/jnwhiteh/vim-golang.git;
+        git clone https://github.com/fatih/vim-go.git;
+        git clone https://github.com/leafgarland/typescript-vim.git;
+        git clone https://github.com/wting/rust.vim.git;
         # utility
-        git clone git://github.com/Shougo/neocomplcache.git;
-        git clone git://github.com/thinca/vim-quickrun.git;
-        git clone git://github.com/Shougo/unite.vim.git;
-        git clone git://github.com/thinca/vim-ref.git;
-        git clone git://github.com/vim-scripts/sudo.vim.git;
+        git clone https://github.com/Shougo/neocomplcache.git;
+        git clone https://github.com/thinca/vim-quickrun.git;
+        git clone https://github.com/Shougo/unite.vim.git;
+        git clone https://github.com/thinca/vim-ref.git;
+        git clone https://github.com/vim-scripts/sudo.vim.git;
         ;;
 
     'upgrade')
@@ -50,5 +49,3 @@ Usage:
 EOM
         ;;
 esac
-
-
